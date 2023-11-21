33f22baab07055d0d82b20e0fb5151a6db8540cb
S-SIRIUS <sirius5b8b@gmail.com>
2023-11-21 15:50:01 +0900

    [fix] Text_Preprocessing 모듈 수정(연속된 개행 하나로)

--- services/SolutionModelApi/Text_Preprocessing/Text_Preprocessing.py
+++ services/SolutionModelApi/Text_Preprocessing/Text_Preprocessing.py
@@ -1,10 +1,13 @@
 import re
 
+
 def Text_Preprocessing(text):
     # 따옴표 제거
     text = text.replace("'","")
     text = text.replace('"',"")
 
+    # 연속된 개행 하나의 개행으로 바꿈
+    text = re.sub(r'(\r\n)+', '\r\n', text)
     # 문자와 개행 사이에 있는 공백 전부 제거
     corrected_text = re.sub(r'\s+\r\n', '\r\n', text)
 
