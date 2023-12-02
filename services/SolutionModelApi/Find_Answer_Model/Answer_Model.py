6e14fa8b579b52113916c954dfa22bceecee4b50
S-SIRIUS <sirius5b8b@gmail.com>
2023-12-02 16:44:11 +0900

    [fix] turbo로 change

--- services/SolutionModelApi/Find_Answer_Model/Answer_Model.py
+++ services/SolutionModelApi/Find_Answer_Model/Answer_Model.py
@@ -28,7 +28,7 @@ async def async_generate(llm, policy, instruction, index):
 
 # 결과를 인덱스와 함께 수집하고, 이를 다시 정렬
 async def generate_concurrently(df):
-    llm = OpenAI(temperature=0.05, top_p=0.3, model_name='gpt-4')
+    llm = OpenAI(temperature=0.05, top_p=0.3, model_name='gpt-4-1106-preview')
     tasks = [async_generate(llm, df['matched_part'][i], df['instruction'][i], i) for i in range(len(df))]
     results = await asyncio.gather(*tasks)
     # 인덱스를 기준으로 결과를 정렬합니다.
