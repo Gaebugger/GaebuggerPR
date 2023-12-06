eec6cc7280351cf9827af789535622f326b4d1aa
MONTH4 <goei4559@gmail.com>
2023-12-06 19:39:45 +0900

    [chore] git release 준비

--- services/Pripen/src/main/frontend/src/components/header/header.js
+++ services/Pripen/src/main/frontend/src/components/header/header.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
-import '../../assets/fonts/fonts.css';
 import './header.css';
 import logoImage from '../../assets/images/pripen_logo.svg';
 import {useAuth} from '../../contexts/AuthContext';
