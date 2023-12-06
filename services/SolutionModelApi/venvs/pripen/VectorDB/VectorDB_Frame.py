34101cc220bd1ad7598c89d08c5bf863973529d6
S-SIRIUS <sirius5b8b@gmail.com>
2023-12-06 16:48:53 +0900

    [fix] VectorDB Insert (메타데이터 -> namespace)

--- services/SolutionModelApi/venvs/pripen/VectorDB/VectorDB_Frame.py
+++ services/SolutionModelApi/venvs/pripen/VectorDB/VectorDB_Frame.py
@@ -2,7 +2,7 @@ from .Extract_Violation import Extract_Violation
 from .Insert_Violation import Insert_Violation
 
 def VectorDB_Frame(final_answer_text):
-    violation_text = Extract_Violation(final_answer_text)
-    isSuccessful = Insert_Violation(violation_text) # 벡터 DB 삽입 및 성공여부 전달
+    Extract_Violation(final_answer_text)
+    isSuccessful = Insert_Violation() # 벡터 DB 삽입 및 성공여부 전달
 
     return isSuccessful
