3b209d90f9019e14e1fcc2699562ec149a160eb2
S-SIRIUS <sirius5b8b@gmail.com>
2023-12-06 12:13:00 +0900

    [feat] Translator 모듈 추가

--- services/SolutionModelApi/venvs/pripen/Find_Answer_Model/Translator.py
+++ services/SolutionModelApi/venvs/pripen/Find_Answer_Model/Translator.py
@@ -4,6 +4,7 @@ def Translator(korean_text):
     auth_key = os.getenv("DeepL_API_KEY")
     translator = deepl.Translator(auth_key)
 
-    english_text = "("+translator.translate_text(korean_text, target_lang="EN-US")+")"
+    english_text = translator.translate_text(korean_text, target_lang="EN-US").text
+    english_text = "("+english_text+")"
 
     return (korean_text + english_text)
